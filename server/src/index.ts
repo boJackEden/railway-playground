25f6dd82ecc2c8ad1e13e0a25bda2233c74fd0d4
Eden Mazzola <mazzolaeden@gmail.com>
2026-01-06 15:18:12 +0100

    revert everything to basic working app because the issue was my railway domain

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,3 @@
-// ================================
-// 🔴 PROCESS-LEVEL DEBUG HANDLERS
-// ================================
-process.on("exit", (code) => {
-  console.log("🛑 process exit event. code =", code);
-});
-
-process.on("beforeExit", (code) => {
-  console.log("🟡 beforeExit event. code =", code);
-});
-
-process.on("uncaughtException", (err) => {
-  console.error("❌ uncaughtException", err);
-});
-
-process.on("unhandledRejection", (reason) => {
-  console.error("❌ unhandledRejection", reason);
-});
-
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -28,42 +9,36 @@ if (process.env.NODE_ENV !== "production") {
   dotenv.config();
 }
 const app = express();
-
-app.get("/api/health", (_req, res, next) => {
-  console.log('THE HEALTH CHECK IS BEING CALLED');
-  res.status(200).send("ok");
-  next();
-});
-
 app.use(express.json());
 
 
 // API routes
 app.use("/api/auth", authRoutes);
 
+app.get("/api/health", (_req, res, next) => {
+  res.status(200).send("ok");
+  next();
+});
+
 // Serve client build in production
-// if (process.env.NODE_ENV === "production") {
-//   const clientDistPath = path.join(__dirname, "..", "..", "client", "dist");
-//   app.use(express.static(clientDistPath));
-//   console.log('CLIENT BUILD PATH', clientDistPath)
-//   app.get(/^(?!\/api).*/, (_req, res) => {
-//     console.log('IS THIS SERVING ANTYHING OR causing my app to crash?')
-//     res.sendFile(path.join(clientDistPath, "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  const clientDistPath = path.join(__dirname, "..", "..", "client", "dist");
+  app.use(express.static(clientDistPath));
+  app.get(/^(?!\/api).*/, (_req, res) => {
+    res.sendFile(path.join(clientDistPath, "index.html"));
+  });
+}
 
 const PORT = Number(process.env.PORT) || 5050;
-console.log("BOOT NODE_ENV:", process.env.NODE_ENV);
-console.log("BOOT process.env.PORT:", process.env.PORT);
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("LISTENING ON:", PORT);
 });
 
 // Connect AFTER listening so Railway can reach your service
-// mongoose
-//   .connect(process.env.MONGO_URI as string, {
-//     serverSelectionTimeoutMS: 10_000, // fail fast rather than hanging forever
-//   })
-//   .then(() => console.log("Mongo connected"))
-//   .catch((err) => console.error("Mongo connect error:", err));
+mongoose
+  .connect(process.env.MONGO_URI as string, {
+    serverSelectionTimeoutMS: 10_000, // fail fast rather than hanging forever
+  })
+  .then(() => console.log("Mongo connected"))
+  .catch((err) => console.error("Mongo connect error:", err));
