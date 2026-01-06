8886e670006b99e0e19ad6e409db2de93325414c
Eden Mazzola <mazzolaeden@gmail.com>
2026-01-06 14:39:37 +0100

    get rid of route logger

--- server/src/index.ts
+++ server/src/index.ts
@@ -32,10 +32,10 @@ app.use(express.json());
 
 
 // API routes
-app.use((req, _res, next) => {
-  console.log(`[REQ] ${req.method} ${req.url}`);
-  next();
-});
+// app.use((req, _res, next) => {
+//   console.log(`[REQ] ${req.method} ${req.url}`);
+//   next();
+// });
 
 app.use("/api/auth", authRoutes);
 
